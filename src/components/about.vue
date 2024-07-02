67043aa86df047cd2f8c551c7a454fbbd3875bdc
Kevin Castro <kev1n-1997@outlook.com>
2024-07-01 21:52:16 -0400

    changed about text and added some basic css

--- src/components/about.vue
+++ src/components/about.vue
@@ -4,23 +4,27 @@
             <h2>ABOUT</h2>
             <hr />
             <div class="about-inner-container">
-            <div class="about-content content-one">
-                <h3>
-                Front-end focused with <br />
-                full stack experience
-                </h3>
-                <p>
-                As a curious developer, I always find myself learning new things to improve my skills and can adapt to
-                new technologies within a few days.
-                </p>
-                <p>
-                I have most of my experience within the front-end space (HTML, CSS, JavaScript) but lately, I've been
-                working a lot with PHP and MySQL which I like and I find myself pretty confident about.
-                </p>
-            </div>
-            <div class="about-content content-two">
-                <img title="People vector" src="@src/images/2590505.png" alt="Vector of person programming" />
-            </div>
+                <div class="about-content content-one">
+                    <h3>
+                        Full Stack Developer
+                    </h3>
+                    <p>
+                        As a curious developer, I always find myself learning new things to improve my skills and can
+                        adapt to
+                        new technologies within a few days. I have a passion for creating beautiful and functional web
+                        applications.
+                    </p>
+                    <br />
+                    <p>
+                        I have most of my experience with Vue.js, Next.js/React, and Laravel/PHP. I have also worked
+                        with databases like MySQL and MongoDB.
+
+
+                    </p>
+                </div>
+                <div class="about-content content-two">
+                    <img title="People vector" src="@src/images/2590505.png" alt="Vector of person programming" />
+                </div>
             </div>
         </div>
     </section>
@@ -32,6 +36,7 @@
     flex-direction: column;
     justify-content: center;
     align-items: center;
+
     p {
         margin-bottom: 1rem;
         max-width: 600px;
@@ -49,6 +54,7 @@
     text-align: center;
     line-height: 3rem;
 }
+
 .about-inner-container {
     display: flex;
     justify-content: center;
@@ -56,18 +62,27 @@
     margin-top: 2rem;
     width: 1000px;
     max-width: 80%;
+
     h3 {
         font-size: 2rem;
         text-align: center;
         line-height: 2.5rem;
         margin-bottom: 1rem;
     }
+
     img {
         width: 100%;
     }
 }
+
 .about-content {
     flex: 1;
     padding: 2rem;
+}
+
+.content-one {
+    background-color: var(--user-button-cardborder);
+    border-radius: 10px;
+    box-shadow: 0 0 10px var(--user-button-shadow);
 }
 </style>
